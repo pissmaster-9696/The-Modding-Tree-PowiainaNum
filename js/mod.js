f67c7b8fdd4ef508606e6211738f7d559b2856db
pissmaster-9696 <topbruhmoments73@gmail.com>
2025-12-06 22:49:56 -0600

    f

--- js/mod.js
+++ js/mod.js
@@ -6,7 +6,7 @@ let modInfo = {
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new PowiainaNum (10), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
